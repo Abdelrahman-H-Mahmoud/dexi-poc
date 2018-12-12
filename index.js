b75a481a5240acbe1f7bd9a9fb04b951eb8447c4
abdelrahman hussein <abdelrahman.hussein@vodafone.com>
2018-12-12 17:36:50 +0200

    handle incoming data

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const config=require('./config');
+
+const handle_data=require('./handle-data');
+
 let app =express();
 app.use(bodyparser.json());
 
 
 app.post('/api/v1/notify',(req,res)=>{
     let body=req.body;
-    console.log(body);
+    res.sendStatus(200);
+    handle_data(body).then((data)=>{
+        console.log(data);
+    });
 });
 
 
